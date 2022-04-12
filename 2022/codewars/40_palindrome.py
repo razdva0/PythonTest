27b15ddd65a6848750d27239448f2a4698917aa1
Vasiliy Mironov <razdva12game@gmail.com>
2022-04-12 11:01:21 +0800

    not codewars

---
+++ 2022/codewars/40_palindrome.py
@@ -0,0 +1,14 @@
+def is_palindrome(string):
+    tmp = [x for x in string.upper() if 65 <= ord(x) <= 90]
+    return tmp == tmp[::-1]
+
+
+if is_palindrome('ave coder') == False:
+    print('OK')
+else:
+    print(is_palindrome('ave coder'))
+
+if is_palindrome("Madam, I'm Adam") == True:
+    print('OK')
+else:
+    print(is_palindrome("Madam, I'm Adam"))
